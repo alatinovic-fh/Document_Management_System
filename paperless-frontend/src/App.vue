dd7fbc325b03ea0b4409e200b5a400789bf6bb2c
florian <postmotern@gmail.com>
2025-10-22 14:10:33 +0200

    added tailwindcss

--- paperless-frontend/src/App.vue
+++ paperless-frontend/src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
 import DocumentForm from '@/components/DocumentForm.vue'
 import DocumentDetails from '@/components/DocumentDetails.vue'
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
+  <NavBar/>
   <DocumentForm/>
   <DocumentDetails/>
+
+
+
+  <div class="p-8 bg-blue-100 rounded-xl">
+    <h1 class="text-3xl font-bold text-blue-700">
+      Tailwind funktioniert 🎉
+    </h1>
+    <p class="mt-2 text-gray-600">
+      Wenn du diesen Text blau hinterlegt siehst, ist Tailwind aktiv.
+    </p>
+  </div>
 
 
 </template>
---
+++ paperless-frontend/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="bg-gray-500 shadow-md px-4 py-2 flex items-center justify-between">
+    <div class="text-xl font-bold text-gray-800">Paperless</div>
+    <div class="hidden md:flex space-x-6">
+      <a href="#" class="text-black‚ hover:text-blue-600">login</a>
+    </div>
+    <button @click="toggleMenu" class="md:hidden text-gray-600 focus:outline-none">
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>
+  </nav>
+
+  <!-- Mobile Menu -->
+  <div v-if="isOpen" class="md:hidden bg-white shadow-md px-4 pb-4 space-y-2">
+    <a href="#" class="block text-gray-600 hover:text-blue-600">Home</a>
+    <a href="#" class="block text-gray-600 hover:text-blue-600">Blog</a>
+    <a href="#" class="block text-gray-600 hover:text-blue-600">Tools</a>
+    <a href="#" class="block text-gray-600 hover:text-blue-600">About</a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
